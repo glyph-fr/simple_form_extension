a63b47c632582b2343593c6fd283e475d2daae8c
alex@glyph.fr <alex@glyph.fr>
2014-04-07 19:17:42 +0200

    inverse image upload

--- lib/simple_form_extension/inputs/image.rb
+++ lib/simple_form_extension/inputs/image.rb
@@ -3,7 +3,6 @@ class ImageInput < SimpleForm::Inputs::Base
     input_html_options[:class] << "image-upload"
 
     "<div class=\"fileinput fileinput-new\" data-provides=\"fileinput\">
-      <div class=\"fileinput-preview thumbnail\"></div>
       <div class=\"\">
         <button class=\"btn btn-default btn-file\" type=\"button\">
           <span class=\"fileinput-new\">Select image</span>
@@ -12,6 +11,7 @@ class ImageInput < SimpleForm::Inputs::Base
         </button>
         <button class=\"btn btn-danger fileinput-exists pull-right\" data-dismiss=\"fileinput\" type=\"button\"><i class=\"fa fa-times\"></i></button>
       </div>
+      <div class=\"fileinput-preview thumbnail\"></div>
     </div>".html_safe
   end
 end
