ff070319ecbce569e76a6a741035d7d663d6ff3d
vala <gonjo@free.fr>
2014-06-12 10:26:47 +0200

    use custom input namespace instead of overriding form builder

--- lib/simple_form_extension.rb
+++ lib/simple_form_extension.rb
@@ -11,4 +11,4 @@ end
 
 SimpleForm::Inputs::Base.send(:include, SimpleFormExtension::Components::Icons)
 
-require "simple_form_extension/ext/form_builder"
+SimpleForm.custom_inputs_namespaces << 'SimpleFormExtension::Inputs'
