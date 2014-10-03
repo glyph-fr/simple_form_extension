8212f15e64ec4477317a41c70dfc2fe5d069fa9a
vala <gonjo@free.fr>
2014-10-03 16:26:38 +0200

    display current image in ImageInput, try to find a thumb to display or fallback to other styles

--- lib/simple_form_extension/inputs/image_input.rb
+++ lib/simple_form_extension/inputs/image_input.rb
@@ -16,9 +16,28 @@ module SimpleFormExtension
             <button class=\"btn btn-danger fileinput-exists pull-right\" data-dismiss=\"fileinput\" type=\"button\"><i class=\"fa fa-times\"></i></button>
           </div>
           <div class=\"fileinput-preview thumbnail\">
-            <img data-src='holder.js/100%x100%' alt=\"100%x100%\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOTAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTkwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9Ijk1IiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE5MHgxNDA8L3RleHQ+PC9zdmc+\" style=\"height: 100%; width: 100%; display: block;\">
+            #{ image_tag }
           </div>
         </div>".html_safe
+      end
+
+      private
+
+      def image_tag
+        if @builder.object.send(:"#{ attribute_name }?")
+          image_url = @builder.object.send(attribute_name).url(image_style)
+          "<img src=\"#{ image_url }\" style=\"height: 100%; width: 100%; display: block;\">"
+        else
+          "<img data-src='holder.js/100%x100%' alt=\"100%x100%\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOTAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTkwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9Ijk1IiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE5MHgxNDA8L3RleHQ+PC9zdmc+\" style=\"height: 100%; width: 100%; display: block;\">"
+        end
+      end
+
+      def image_style
+        styles = @builder.object.send(attribute_name).styles.map(&:first)
+        # Check if there's a :thumb or :thumbnail style in attachment definition
+        thumb = styles.find { |s| %w(thumb thumbnail).include?(s.to_s) }
+        # Return the potentially smallest size !
+        thumb || styles.first || :original
       end
     end
   end
