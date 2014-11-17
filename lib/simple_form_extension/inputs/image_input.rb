c9cad83b29b8f75172cb14c7ddea5108193c3e78
xana68 <alex@glyph.fr>
2014-11-17 15:33:49 +0100

    remove holder.js for input_file as image

--- lib/simple_form_extension/inputs/image_input.rb
+++ lib/simple_form_extension/inputs/image_input.rb
@@ -28,7 +28,7 @@ module SimpleFormExtension
           image_url = @builder.object.send(attribute_name).url(image_style)
           "<img src=\"#{ image_url }\" style=\"height: 100%; width: 100%; display: block;\">"
         else
-          "<img data-src='holder.js/100%x100%' alt=\"100%x100%\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOTAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTkwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9Ijk1IiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE5MHgxNDA8L3RleHQ+PC9zdmc+\" style=\"height: 100%; width: 100%; display: block;\">"
+          "<div class=\"empty-thumbnail\"></div>"
         end
       end
 
