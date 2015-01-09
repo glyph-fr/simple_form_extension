8f8ddb23f4869c7c43830a5ac7ed834b64221f9a
vala <gonjo@free.fr>
2015-01-09 15:39:08 +0100

    allow removing existing file from file input

--- lib/simple_form_extension/inputs/file_input.rb
+++ lib/simple_form_extension/inputs/file_input.rb
@@ -37,21 +37,42 @@ module SimpleFormExtension
       end
 
       def existing_file_tag
-        return '' unless object.send(:"#{ attribute_name }?")
+        return '' unless has_file?
 
         url = object.send(attribute_name).url
 
-        content_tag(:div, class: 'input-group help-block existing-file') do
+        content_tag(:div, class: 'input-group help-block existing-file', data: { provides: 'existing-file'}) do
           content_tag(:span, class: 'input-group-addon') do
             "#{ _translate('file.existing_file') } : ".html_safe
           end +
 
-          content_tag(:a, class: 'btn btn-default ', href: url, target: '_blank') do
-            content_tag(:i, '', class: 'fa fa-file') +
-            "&nbsp;".html_safe +
-            object.send(:"#{ attribute_name }_file_name").html_safe
+          content_tag(:div, class: 'btn-group') do
+            content_tag(:a, class: 'btn btn-default ', href: url, target: '_blank', data: { toggle: 'existing-file' }) do
+              content_tag(:i, '', class: 'fa fa-file') +
+              "&nbsp;".html_safe +
+              object.send(:"#{ attribute_name }_file_name").html_safe
+            end +
+
+            remove_file_button
           end
         end
+      end
+
+      def remove_file_button
+        return unless object.respond_to?(:"#{ remove_attachment_method }=")
+
+        content_tag(:button, class: 'btn btn-danger', type: 'button', data: { dismiss: 'existing-file' }) do
+          content_tag(:i, '', class: 'fa fa-remove', data: { :'removed-class' => 'fa fa-refresh' }) +
+          @builder.hidden_field(remove_attachment_method, class: 'remove-file-input', value: nil)
+        end
+      end
+
+      def remove_attachment_method
+        options[:remove_method] || :"remove_#{ attribute_name }"
+      end
+
+      def has_file?
+        @has_file ||= object.send(:"#{ attribute_name }?")
       end
     end
   end
