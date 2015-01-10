da2fb67db517cc0fae2911fe08098d929975148b
vala <gonjo@free.fr>
2015-01-10 23:03:35 +0100

    refactore date_time input html building, date format translations, and add default value to field to match js plugin

--- lib/simple_form_extension/inputs/date_time_input.rb
+++ lib/simple_form_extension/inputs/date_time_input.rb
@@ -3,24 +3,33 @@ module SimpleFormExtension
     class DateTimeInput < SimpleForm::Inputs::DateTimeInput
       include SimpleFormExtension::Translations
 
+      delegate :content_tag, to: :template
+
       def input(wrapper_options = nil)
       	input_html_options[:class] << "form-control"
 
         input_html_options[:data] ||= {}
         input_html_options[:data].merge!(
-          :'date-format' => _translate('date.format'),
-          :'datetime-format' => _translate('datetime.format'),
+          :'date-format' => _translate('date.format.js'),
+          :'datetime-format' => _translate('datetime.format.js'),
           :'week-start-day' => _translate('shared.week_start_day')
         )
 
-        "<div class=\"input-group\">
-          #{@builder.text_field(attribute_name, input_html_options)}
-          <span class=\"input-group-btn\">
-            <button type=\"button\" class=\"btn btn-default datetimepicker-trigger\">
-              <i class=\"fa fa-calendar\"></i>
-            </button>
-          </span>
-        </div> ".html_safe
+        if (value = object.send(attribute_name)) && !input_html_options.key?(:value)
+          format = _translate("#{ input_type }.format.rails")
+          input_html_options[:value] = I18n.l(value, format: format)
+        end
+
+
+        content_tag(:div, class: 'input-group') do
+          @builder.text_field(attribute_name, input_html_options) +
+
+          content_tag(:span, class: 'input-group-btn') do
+            content_tag(:button, class: 'btn btn-default datetimepicker-trigger') do
+              content_tag(:i, '', class: 'fa fa-calendar')
+            end
+          end
+        end
       end
     end
   end
