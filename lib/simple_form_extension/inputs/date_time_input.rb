647eb7598740f92b4b191cc4efcac3ed7ae8f7fe
xana68 <alex@glyph.fr>
2015-02-20 17:31:22 +0100

    add time picker

--- lib/simple_form_extension/inputs/date_time_input.rb
+++ lib/simple_form_extension/inputs/date_time_input.rb
@@ -9,27 +9,38 @@ module SimpleFormExtension
       	input_html_options[:class] << "form-control"
 
         input_html_options[:data] ||= {}
-        input_html_options[:data].merge!(
-          :'date-format' => _translate('date.format.js'),
-          :'datetime-format' => _translate('datetime.format.js'),
-          :'week-start-day' => _translate('shared.week_start_day')
-        )
+        input_html_options[:data].merge!(type_specific_option)
 
         if (value = object.send(attribute_name)) && !input_html_options.key?(:value)
           format = _translate("#{ input_type }.format.rails")
           input_html_options[:value] = I18n.l(value, format: format)
         end
 
-
         content_tag(:div, class: 'input-group') do
           @builder.text_field(attribute_name, input_html_options) +
 
           content_tag(:span, class: 'input-group-btn') do
             content_tag(:button, type: 'button', class: 'btn btn-default datetimepicker-trigger') do
-              content_tag(:i, '', class: 'fa fa-calendar')
+              content_tag(:i, '', class: "fa fa-#{ icon }")
             end
           end
         end
+      end
+
+      private
+
+      def type_specific_option
+        options = { format: _translate("#{ input_type }.format.js") }
+
+        unless input_type == :time 
+          options[:'week-start-day'] = _translate('shared.week_start_day')
+        end
+
+        options
+      end
+
+      def icon
+        input_type == :time ? 'clock-o' : 'calendar'
       end
     end
   end
