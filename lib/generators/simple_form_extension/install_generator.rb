72a069f0f1bd5aec1c51ae15bdb2fab8dd63e971
vala <gonjo@free.fr>
2015-05-26 10:35:06 +0200

    add custom redactor-rails gem to Gemfile on installation

--- lib/generators/simple_form_extension/install_generator.rb
+++ lib/generators/simple_form_extension/install_generator.rb
@@ -12,6 +12,16 @@ module SimpleFormExtension
         template "config/locales/simple_form_extension.en.yml"
         template "config/locales/simple_form_extension.fr.yml"
       end
+
+      def add_custom_redactor_gem
+        gem 'redactor-rails', github: 'glyph-fr/redactor-rails'
+      end
+
+      def bundle
+        Bundler.with_clean_env do
+          run 'bundle install'
+        end
+      end
     end
   end
 end
